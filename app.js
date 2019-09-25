90c22bb02c283e58044b069a5ea29b301a096ff9
MasterTres <tresbednarz@gmail.com>
2019-09-25 14:46:11 -0400

    oops

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const twoNums = ()=>{
-    let one = document.getElementById("numOne");
-    let two = document.getElementById("numTwo");
     let max = 99;
     let nums = [];
     for(let i = 0; i < 2; i++){
+        let one = document.getElementById("numOne");
+        let two = document.getElementById("numTwo");
         let num = Math.floor(Math.random()* Math.floor(max))
         if(num != 0){
             nums.push(num) 
         }else{
-            return twoNums()
+            return twoNums;
         }
         one.innerHTML = nums[0];
        two.innerHTML = nums[1];
