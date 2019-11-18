17acebeb894de32f0346d5775fc422ec7d2fc5f0
MasterTres <tresbednarz@gmail.com>
2019-11-18 16:34:33 -0500

    trying to update interval

---
+++ public/app.js
@@ -0,0 +1,52 @@
+let slider = document.getElementById("slider-range").value;
+
+const twoNums = () => {
+  let max = 99;
+  let nums = [];
+  for (let i = 0; i < 2; i++) {
+    let one = document.getElementById("numOne");
+    let two = document.getElementById("numTwo");
+    let num = Math.floor(Math.random() * Math.floor(max));
+    if (num != 0) {
+      nums.push(num);
+    } else {
+      return twoNums;
+    }
+    one.innerHTML = nums[0];
+    two.innerHTML = nums[1];
+  }
+};
+setInterval(twoNums, 4000); //<-- change 4000 to slider value from above
+
+const operator = document.getElementById("operation");
+const changeAdd = () => {
+  if (operator.innerHTML == "+") {
+    return true;
+  } else {
+    operator.innerHTML = "+";
+  }
+};
+
+const changeSub = () => {
+  if (operator.innerHTML == "-") {
+    return true;
+  } else {
+    operator.innerHTML = "-";
+  }
+};
+
+const changeMult = () => {
+  if (operator.innerHTML == "*") {
+    return true;
+  } else {
+    operator.innerHTML = "*";
+  }
+};
+
+const changeDiv = () => {
+  if (operator.innerHTML == "/") {
+    return true;
+  } else {
+    operator.innerHTML = "/";
+  }
+};
---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+const publicDir = path.join(__dirname, "../public");
+
+app.use(express.static(publicDir));
+
+app.get("", (req, res) => {
+  res.render("index", {});
+});
+
+app.listen(port, () => {
+  console.log(`server listening on port: ${port}`);
+});
