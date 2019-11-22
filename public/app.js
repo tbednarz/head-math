4858fd28b44998f44d7972325646d2a085125705
Tres <tresbednarz@gmail.com>
2019-11-22 14:07:30 -0500

    added operation changes and answer toggle

--- public/app.js
+++ public/app.js
@@ -1,8 +1,11 @@
 let slider = document.getElementById("slider-range");
 
+//Generates two numbers between 1-100
+//pushes too an array
+//then pushes into html
 const twoNums = () => {
-  let max = 99;
   let nums = [];
+  let max = 99;
   for (let i = 0; i < 2; i++) {
     let one = document.getElementById("numOne");
     let two = document.getElementById("numTwo");
@@ -15,13 +18,28 @@ const twoNums = () => {
     one.innerHTML = nums[0];
     two.innerHTML = nums[1];
   }
+  let answer = nums => {
+    let answerBox = document.getElementById("answer");
+    if (operator.innerHTML == "+") {
+      answerBox.innerHTML = nums[0] + nums[1];
+    } else if (operator.innerHTML == "-") {
+      answerBox.innerHTML = nums[0] - nums[1];
+    } else if (operator.innerHTML == "*") {
+      answerBox.innerHTML = nums[0] * nums[1];
+    } else {
+      answerBox.innerHTML = nums[0] / nums[1];
+    }
+  };
+  return answer(nums);
 };
+//event listener to see if slider was used to change timing
 let timerId = setInterval(twoNums, 4000); //<-- change 4000 to slider value from above
 slider.addEventListener("change", function() {
   clearInterval(timerId);
   timerId = setInterval(twoNums, slider.value);
 });
 
+//Functions to change operators
 const operator = document.getElementById("operation");
 const changeAdd = () => {
   if (operator.innerHTML == "+") {
